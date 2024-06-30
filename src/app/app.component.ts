4bbf11ed99a6bb1c0735fb9724476b46cefe8495
Ravi N <your.email@example.com>
2024-06-30 08:53:58 +0530

    ngIf hiding and show button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-17-directives-new';
+  display = true;
+  num = -20;
+
+  showVariable:boolean = false;
+
+  show() {
+    this.showVariable = !this.showVariable;
+  }
+  
+
 }
