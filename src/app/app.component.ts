4e169c6a4810ee93c63051f2d653a0c682786ea1
Ravi N <your.email@example.com>
2024-06-30 12:59:08 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface employeeInterface {
+  id : number;
+  name : string;
+  role: string;
+  age : number
+
+};
 
 @Component({
   selector: 'app-root',
@@ -16,5 +24,63 @@ export class AppComponent {
     this.showVariable = !this.showVariable;
   }
   
+  fullStackDev = [{
+    id: 1,
+    name: 'Angular'
+  },
+  {
+    id: 2,
+    name: 'React'
+  },
+  {
+    id: 3,
+    name: 'Vue'
+  },
+  {
+    id: 4,
+    name: 'Node'
+  }];
+
+  listItems = [{
+    name: 'Ravi',
+    age: 22
+  },
+  {
+    name: 'John',
+    age: 21
+  },
+  {
+    name: 'Smith',
+    age: 25
+  },
+  {
+    name: 'Allen',
+    age: 28
+  },
+  {
+    name: 'Morkel',
+    age: 26
+  }]
+
+  displayList:boolean = false;
+  showList() {
+    this.displayList = !this.displayList;
+  }
+
+
+  employees = signal<employeeInterface[]>([
+    {id: 1, name: 'Employee A', role: 'Admin', age: 35},
+    {id: 2, name: 'Employee B', role: 'Manager', age: 40},
+    {id: 3, name: 'Employee C', role: 'Developer', age: 25},
+    {id: 4, name: 'Employee D', role: 'HR', age: 28}
+  ]);
+
+  age = 0;
+  isEligible: boolean = false;
+
+  checkEligibility() {
+    this.isEligible = this.age >= 18;
+  }
+
 
 }
